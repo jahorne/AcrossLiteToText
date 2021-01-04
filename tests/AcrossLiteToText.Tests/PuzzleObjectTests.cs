7fae4ed1d866eca50b58b3d1cdaa380b2c72912a
Ajay Saxena <ajsaxena@microsoft.com>
2021-01-03 23:48:02 -0800

    Added test infra

---
+++ tests/AcrossLiteToText.Tests/PuzzleObjectTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace AcrossLiteToText.Tests
+{
+    public class PuzzleObjectTests
+    {
+        [Fact]
+        // TODO write real test.
+        public void DummyTest()
+        {
+            Assert.True(true, "This is a dummy test");
+        }
+    }
+}
